1a9f5ca1e98f66a1e4a5bdd490b23787d032a00a
Nathan James <minthihaaung950@gmail.com>
2024-10-27 01:40:35 +0630

    Extension method(typeInfo)

---
+++ lib/test3_extensionMethod.dart
@@ -0,0 +1,25 @@
+// Dart Programming language မှာရှိတဲ့ class အားလုံးမှာ သက်ရောက်မယ့် typeInfo လို့ အမည်ရတဲ့
+// extension method ၁ ခုဖန်တီးပေးပါ။ အဲ့ဒီ method ရဲ့ အလုပ်ကတော့ class ရဲ့ type info ကိုပြပေးရပါမယ်။
+// Eg.
+// true.typeInfo(); // runtimeType
+// Output :
+// This is bool data type
+void main() {
+  // boolean
+  true.TypeInfo();
+  // number(int)
+  int number = 10;
+  number.TypeInfo();
+  // String
+  String name = 'Thaw Zin';
+  name.TypeInfo();
+  // double
+  double length = 10.4;
+  length.TypeInfo();
+}
+
+extension ext on Object {
+  void TypeInfo() {
+    print(this.runtimeType.toString());
+  }
+}
